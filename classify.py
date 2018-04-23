d3e158792a4ee885132bcb605c6de36d5a13f2de
Simon S.K. Lee <robot39.redcap@gmail.com>
2018-04-24 00:17:18 +0800

    Create classify.py

---
+++ classify.py
@@ -0,0 +1,39 @@
+# image import
+
+from sparkdl import readImages
+from pyspark.sql.functions import lit
+
+img_dir = "hdfs:///flower-classify/personalities"
+
+#Read images and Create training & test DataFrames for transfer learning
+jobs_df = readImages(img_dir + "/jobs").withColumn("label", lit(1))
+zuckerberg_df = readImages(img_dir + "/zuckerberg").withColumn("label", lit(0))
+jobs_train, jobs_test = jobs_df.randomSplit([0.6, 0.4])
+zuckerberg_train, zuckerberg_test = zuckerberg_df.randomSplit([0.6, 0.4])
+
+#dataframe for training a classification model
+train_df = jobs_train.unionAll(zuckerberg_train)
+
+#dataframe for testing the classification model
+test_df = jobs_test.unionAll(zuckerberg_test)
+
+# model creation
+
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml import Pipeline
+from sparkdl import DeepImageFeaturizer
+
+featurizer = DeepImageFeaturizer(inputCol="image", outputCol="features", modelName="InceptionV3")
+lr = LogisticRegression(maxIter=20, regParam=0.05, elasticNetParam=0.3, labelCol="label")
+p = Pipeline(stages=[featurizer, lr])
+p_model = p.fit(train_df)
+
+# model tester
+
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+df = p_model.transform(test_df)
+df.show()
+
+predictionAndLabels = df.select("prediction", "label")
+evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
+print("Training set accuracy = " + str(evaluator.evaluate(predictionAndLabels)))
