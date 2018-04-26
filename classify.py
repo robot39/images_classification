7643dd9c6d5afb0af1c9519693a0070642931bfd
Simon S.K. Lee <robot39.redcap@gmail.com>
2018-04-26 22:53:48 +0800

    Update classify.py

--- classify.py
+++ classify.py
@@ -7,20 +7,21 @@ spark = SparkSession(sc)
 from sparkdl import readImages
 from pyspark.sql.functions import lit
 
-img_dir = "hdfs:///flower-classify/personalities"
-model_path = "hdfs:///flower-classify/persons.model"
+img_dir = "hdfs:///flower-classify/flowers"
 
 #Read images and Create training & test DataFrames for transfer learning
-jobs_df = readImages(img_dir + "/jobs").withColumn("label", lit(1))
-zuckerberg_df = readImages(img_dir + "/zuckerberg").withColumn("label", lit(0))
-jobs_train, jobs_test = jobs_df.randomSplit([0.6, 0.4])
-zuckerberg_train, zuckerberg_test = zuckerberg_df.randomSplit([0.6, 0.4])
+daisy_df = readImages(img_dir + "/daisy").withColumn("label", lit(0))
+dandelion_df = readImages(img_dir + "/dandelion").withColumn("label", lit(1))
+roses_df = readImages(img_dir + "/roses").withColumn("label", lit(2))
+sunflowers_df = readImages(img_dir + "/sunflowers").withColumn("label", lit(3))
+tulips_df = readImages(img_dir + "/tulips").withColumn("label", lit(4))
 
 #dataframe for training a classification model
-train_df = jobs_train.unionAll(zuckerberg_train)
+train_df = daisy_train.unionAll(dandelion_train).unionAll(roses_train).unionAll(sunflowers_train).unionAll(tulips_train)
+print("TRAIN DF PREPARED.")
 
 #dataframe for testing the classification model
-test_df = jobs_test.unionAll(zuckerberg_test)
+test_df = daisy_test.unionAll(dandelion_test).unionAll(roses_test).unionAll(sunflowers_test).unionAll(tulips_test)
 
 # model creation
 
@@ -29,20 +30,17 @@ from pyspark.ml import Pipeline
 from sparkdl import DeepImageFeaturizer
 
 featurizer = DeepImageFeaturizer(inputCol="image", outputCol="features", modelName="InceptionV3")
-lr = LogisticRegression(maxIter=20, regParam=0.05, elasticNetParam=0.3, labelCol="label")
+lr = LogisticRegression(maxIter=5, regParam=0.05, elasticNetParam=0.3, labelCol="label")
 p = Pipeline(stages=[featurizer, lr])
-p.save(model_path)
 p_model = p.fit(train_df)
-p_model_loaded = p_model
 
 # model tester
 
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-p_model_loaded = LogisticRegressionModel.read.load(model_path)
-df = p_model_loaded.transform(test_df)
-df.show()
 
+df = p_model.transform(test_df)
+df.cache()
+df.show()
 predictionAndLabels = df.select("prediction", "label")
 evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
-#predictions.select("filePath", "Prediction").show(truncate=False)
 print("Training set accuracy = " + str(evaluator.evaluate(predictionAndLabels)))
