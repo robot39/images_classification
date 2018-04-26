d2bbdb61d587e0062859104ca3aedecf8eb63777
Simon S.K. Lee <robot39.redcap@gmail.com>
2018-04-26 23:34:41 +0800

    Update classify.py

--- classify.py
+++ classify.py
@@ -30,7 +30,7 @@ from pyspark.ml import Pipeline
 from sparkdl import DeepImageFeaturizer
 
 featurizer = DeepImageFeaturizer(inputCol="image", outputCol="features", modelName="InceptionV3")
-lr = LogisticRegression(maxIter=5, regParam=0.05, elasticNetParam=0.3, labelCol="label")
+lr = LogisticRegression(maxIter=20, regParam=0.05, elasticNetParam=0.3, labelCol="label")
 p = Pipeline(stages=[featurizer, lr])
 p_model = p.fit(train_df)
 
